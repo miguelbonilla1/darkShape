cfbe04f8ea6d2101c257e52422bd1bbb1ab050a1
Miguel Bonilla <miguelbonilla1886@gmail.com>
2024-09-16 03:27:32 -0400

    text fix

--- src/components/Cursos.jsx
+++ src/components/Cursos.jsx
@@ -14,43 +14,41 @@ const Cursos = ({ cursos }) => {
         <div className="absolute inset-0 bg-black opacity-30"></div>
 
         {/* Contenido */}
-        <div className="relative z-10 w-full max-w-screen-xl px-5 py-10">
+        <div className="relative z-10 w-full max-w-screen-xl px-6 py-10 md:py-20 lg:py-24">
           {/* Título */}
           <div className="text-center mb-8">
-            <h1 className="text-5xl font-bold mb-5" style={{ textShadow: "2px 2px 5px rgba(0, 0, 0, 0.5)" }}>
+            <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold mb-6" style={{ textShadow: "2px 2px 5px rgba(0, 0, 0, 0.5)" }}>
               Cursos
             </h1>
           </div>
 
           {/* Descripción */}
-          {/* Descripción */}
-          <div className="mb-10 px-1 pl-5">
-            <p className="text-xl sm:text-2xl md:text-3xl lg:text-3xl text-gray-200 leading-relaxed px-5 pl-5 ">
-              En darkshapeStudios tenemos una variedad de cursos que puedes hallar en nuestro grupo privado. Únete para mejorar tu dominio en grabación de audio, mezcla y mastering.
+          <div className="mb-12 px-4 md:px-12 lg:px-11 lg:ml-10">
+            <p className="text-xl sm:text-2xl md:text-3xl lg:text-3xl text-gray-200 leading-relaxed sm:text-center md:text-left px-3 sm:px-0 pl-3">
+              En Dark Shape Studios tenemos una variedad de cursos que puedes hallar en nuestro grupo privado. Únete para mejorar tu dominio en grabación de audio, mezcla y mastering.
             </p>
           </div>
 
-
           {/* Listado de Cursos */}
-          <div className="flex flex-col md:flex-row md:flex-wrap justify-center gap-5">
+          <div className="flex flex-col md:flex-row md:flex-wrap justify-center gap-6">
             {cursos.map((curso, index) => (
               <div
                 key={index}
-                className="bg-gray-900 bg-opacity-75 rounded-lg p-6  shadow-lg w-full md:w-1/3 px-10"
+                className="bg-gray-900 bg-opacity-75 rounded-lg p-6 shadow-lg w-full md:w-5/12 lg:w-1/3"
               >
                 {/* Título del Curso */}
-                <h2 className=" pl-3 text-2xl font-bold mb-4">{curso.nombre}</h2>
+                <h2 className="text-2xl font-bold mb-4 text-center md:text-left">{curso.nombre}</h2>
 
                 {/* Descripción del Curso */}
-                <p className=" pl-3 text-xl text-gray-200 font-roboto mb-6">{curso.descripcion}</p>
+                <p className="text-lg text-gray-200 font-roboto mb-6 text-center md:text-left">{curso.descripcion}</p>
 
                 {/* Botón e Imagen */}
-                <div className="flex flex-col sm:flex-row items-center justify-center space-y-4 sm:space-y-0 sm:space-x-4">
+                <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-between space-y-4 sm:space-y-0 px-11">
                   <button className="bg-blue-500 text-white py-3 px-6 rounded-lg text-lg hover:bg-blue-700">
                     Inscríbete
                   </button>
                   <img
-                    className="sm:ml-2 mt-2 sm:mt-0"
+                    className="sm:ml-4 mt-2 sm:mt-0"
                     style={{ height: "100px", width: "100px", filter: "brightness(0) invert(1)" }}
                     src={manito}
                     alt="Manito"
